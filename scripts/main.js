10469b0b8ea17cb948f6f087a9b48b8f01faf02a
memonamadi <madi.memona@gmail.com>
2015-08-07 07:35:49 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+(function getImages() {
+	var baseUrl, url, flickrMethod, apiKey, userId, privacy_filter, format;
+
+	baseUrl = "https://api.flickr.com/services/rest/?",
+	flickrMethod = "method=flickr.people.getPhotos",
+	apiKey = "&api_key=8e60c6375f7a67331221eef287e89570",
+	userId = "&user_id=133997903%40N08",
+	privacy_filter = "&privacy_filter=1",
+	format = "&format=json&nojsoncallback=1",
+	URL = baseUrl + flickrMethod + apiKey + userId + privacy_filter + format;
+
+	$.getJSON(URL, function(data) {
+		$.each(data.photos.photo, function(i, item) {
+			var img_src, img_url, img_thumbnail;
+
+			img_src = "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_c.jpg";
+			img_url = "https://www.flickr.com/photos/" + item.owner + "/" + item.id;
+			img_thumbnail = ("<a href='" + img_url + "'>");
+			img_thumbnail += ("<img src='" + img_src + "'/>");
+			img_thumbnail += ("</a>");
+			$(img_thumbnail).appendTo(".grid-container");
+		});
+	});
+	
+})();
